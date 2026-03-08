b6104a9e29e24c0055af73095e48c93a3d024e49
Chris Lockwood <35789363+slow-boat@users.noreply.github.com>
2026-03-08 15:48:02 +0800

    fix dawn watchdog- ubus timeout check return code 7 was coming back as 249 when started by procd (unsigned char version of -7)

--- net/dawn/files/dawn-wdog.sh
+++ net/dawn/files/dawn-wdog.sh
@@ -4,7 +4,9 @@ while true; do
 	sleep 30
 
 	ubus -t 5 call dawn get_network 2>/dev/null
-	[ $? == 7 ] || continue
+	rc=$?
+	# +/-7 return value is timeout
+	[ $rc == 7 ] || [ $rc == 249 ] || continue
 	
 	logger -t dawn-watchdog -p daemon.err "dawn deadlocked- force restart"
 	/etc/init.d/dawn restart
